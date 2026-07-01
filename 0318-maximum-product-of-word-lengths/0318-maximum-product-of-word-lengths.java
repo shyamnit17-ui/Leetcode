372d1ae91b7d3093c0552e1dc2cbc5119521d258
Shyam Rathod <shyamnit17@gmail.com>
2026-07-01 12:00:39 +0530

    Time: 15 ms (27.26%), Space: 48.7 MB (86.26%) - LeetHub

--- 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.java
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.java
@@ -20,7 +20,7 @@ class Solution {
             int maximum=0;
             int masks[]=getmasks(words);
            for(int i=0;i<words.length;i++){
-            for(int j=0;j<words.length;j++){
+            for(int j=i+1;j<words.length;j++){
                 if((masks[i]&masks[j])==0){
                     maximum=Math.max(maximum,words[i].length()*words[j].length());
                 }
