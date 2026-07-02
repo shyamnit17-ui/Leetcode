7cfef4cb327a6772d3ec18a9e203a96cdaeceb97
Shyam Rathod <shyamnit17@gmail.com>
2026-07-02 16:25:51 +0530

    Time: 981 ms (5.01%), Space: 49.9 MB (34.25%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,36 @@
+class Solution {
+    private String generateFingerprint(String s){
+        int arr[]=new int[26];
+       
+        StringBuilder sb = new StringBuilder("");
+         for(int i=0;i<s.length();i++){
+           arr[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(arr[i]!=0){
+                sb.append((char)(i+'a'));
+                sb.append(arr[i]);
+            }
+        }
+        return sb.toString();
+    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> ans= new ArrayList<>();
+        HashMap<String,List<String>> stres= new HashMap<>();
+        for(int i=0;i<strs.length;i++){
+        String key= generateFingerprint(strs[i]);
+        if(!stres.containsKey(generateFingerprint(strs[i]))){
+            List<String> sl= new ArrayList<>();
+             stres.put(key,sl);
+             sl.add(strs[i]);
+         }
+         else if(stres.containsKey(key)){
+            stres.get(key).add(strs[i]);
+         } 
+        if(!ans.contains(stres.get(key))) {
+            ans.add(stres.get(key));
+        }
+        }
+        return ans;
+    }
+}
