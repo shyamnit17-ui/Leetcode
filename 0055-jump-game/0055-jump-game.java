a75a374e5c67bdfe23b38e3212239a3ebde33504
Shyam Rathod <shyamnit17@gmail.com>
2026-07-16 12:09:37 +0530

    Time: 2 ms (88.99%), Space: 47.8 MB (63.08%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean canJump(int[] nums) {
      int currentfarthestreach=0;
        for(int i=0;i<nums.length;i++){
-     if(i>currentfarthestreach){
+       if(i>currentfarthestreach){
             return false;
         }   
          if(nums[i]+i>currentfarthestreach){
