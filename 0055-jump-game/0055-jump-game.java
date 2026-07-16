f5006855639ed1afdbd25346536ea716b30670f3
Shyam Rathod <shyamnit17@gmail.com>
2026-07-16 12:07:15 +0530

    Time: 2 ms (88.99%), Space: 48.2 MB (9.02%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+     int currentfarthestreach=0;
+       for(int i=0;i<nums.length;i++){
+     if(i>currentfarthestreach){
+            return false;
+        }   
+         if(nums[i]+i>currentfarthestreach){
+           currentfarthestreach=nums[i]+i;
+        }
+       
+        }
+        return true;
+    }
+}
