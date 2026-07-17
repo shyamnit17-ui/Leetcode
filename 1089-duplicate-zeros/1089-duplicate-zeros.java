d3f3476f13a3fa0adae94e54cb09d54638d75356
Shyam Rathod <shyamnit17@gmail.com>
2026-07-17 19:49:37 +0530

    Time: 1 ms (99.38%), Space: 47 MB (39.97%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,17 +1,33 @@
 class Solution {
-    public static void shiftRightFrom(int[] arr, int targetIndex) {
-    
-    for (int i = arr.length - 2; i >= targetIndex; i--) {
-        arr[i + 1] = arr[i];
-    }
-}
-
     public void duplicateZeros(int[] arr) {
-      for(int i=0;i<arr.length-1;i++){
-        if(arr[i]==0){
-           shiftRightFrom(arr,i);
-           i++;
-        }
-        }
+       int x=arr.length;
+       for(int i=0;i<arr.length;i++){
+        if(i!= arr.length &&arr[i]==0){
+            x+=1;
+         }
+       }
+       int j=x-1;
+       int i=arr.length-1;
+       
+       while(i>=0){
+          if(arr[i]!=0){
+            if(0 <= j && j < arr.length){
+                arr[j]=arr[i];
+            }
+            j--;
+          }
+          else{
+            if(0 <= j && j < arr.length){
+                 arr[j]=0; 
+            }
+             j--;
+           if(0 <= j && j< arr.length){
+             arr[j]=0;
+            }
+              j--;
+          }
+          i--;
+       }
+      
     }
 }
