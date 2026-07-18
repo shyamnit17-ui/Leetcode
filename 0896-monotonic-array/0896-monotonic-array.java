b0dc9d0a9b292270d226b38a0057f96cb82695fe
Shyam Rathod <shyamnit17@gmail.com>
2026-07-18 18:03:32 +0530

    Time: 1 ms (96.07%), Space: 84.8 MB (96.94%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,23 +1,22 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
 
-    boolean bool=true;
-    boolean bool2=true;
-    for(int i=0;i<nums.length-1;i++){
-    //monotonic increasing
-     if(nums[i]<nums[i+1]){
-        bool=false;
-     }
-     //monotonic decreasing
-    else if(nums[i]>nums[i+1]){
-        bool2=false;
-     }
-     else{
-        if(bool==false &&bool2==false){
-             return false;
+        boolean bool = true;
+        boolean bool2 = true;
+        for (int i = 0; i < nums.length - 1; i++) {
+            //monotonic increasing
+            if (nums[i] < nums[i + 1]) {
+                bool = false;
+            }
+            //monotonic decreasing
+            else if (nums[i] > nums[i + 1]) {
+                bool2 = false;
+            }
+
+            if (bool == false && bool2 == false) {
+                return false;
+            }
         }
-     }
+        return bool || bool2;
     }
-     return bool||bool2;  
-}
 }
