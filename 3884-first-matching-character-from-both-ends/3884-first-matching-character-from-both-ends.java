c5a759ff3d8ce5d52bcdf7e7a10e8745bb982e00
Shyam Rathod <shyamnit17@gmail.com>
2026-07-24 19:54:03 +0530

    Time: 0 ms (100%), Space: 43.7 MB (15.73%) - LeetHub

--- 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.java
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.java
@@ -1,6 +1,6 @@
 class Solution {
     public int firstMatchingIndex(String s) {
-        for(int i=0;i<s.length();i++){
+        for(int i=0;i<s.length()+1/2;i++){
             if(s.charAt(i)==s.charAt(s.length()-i-1)){
                 return i;
             }
