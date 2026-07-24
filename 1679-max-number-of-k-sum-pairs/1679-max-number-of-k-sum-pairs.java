5b9d0b9e751bd38927a270f0fe91357db78816cc
Shyam Rathod <shyamnit17@gmail.com>
2026-07-24 17:15:38 +0530

    Time: 51 ms (5.01%), Space: 77.2 MB (5.01%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,21 +1,38 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        Arrays.sort(nums);
+        HashMap<Integer,Integer> set= new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            set.put(nums[i],set.getOrDefault(nums[i],0)+1);
+        }
         int i=0;
-        int j=nums.length-1;
         int count=0;
-        while(i<j){
-            if(nums[i]+nums[j]==k){
-                count++;
-                i++;
-                j--;
+        while(i<nums.length){
+            int num=nums[i];
+            int comp=k-nums[i];
+          if(num!=comp){
+            if (set.getOrDefault(num, 0) > 0 &&set.getOrDefault(comp, 0) > 0){          
+            set.put(num,set.getOrDefault(num,0)-1);
+            set.put(comp,set.getOrDefault(comp,0)-1);
+            count++;
+             if (set.getOrDefault(num, 0) == 0 ){
+                set.remove(num);
+             }
+              if (set.getOrDefault(comp, 0) ==0 ){
+                set.remove(comp);
+              }
+           }
+          }
+          else{
+            if(set.getOrDefault(num,0)>=2){
+              set.put(num,set.getOrDefault(num,0)-2);
+              count++;
+              if (set.getOrDefault(num, 0) == 0 ){
+                set.remove(num);
+              }
+             
             }
-           else if(nums[i]+nums[j]<k){
-                i++;
-            }
-            else{
-                j--;
-            }
+          }
+          i++;     
         }
         return count;
     }
