4051cf6a501a9a7f171ee27049601b2c309fd2aa
Shyam Rathod <shyamnit17@gmail.com>
2026-08-03 23:31:07 +0530

    Time: 0 ms (100%), Space: 47.2 MB (25.43%) - LeetHub

---
+++ 0665-non-decreasing-array/0665-non-decreasing-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length-1;i++){
+
+            if(nums[i]>nums[i+1]){
+               if(i>0&&nums[i-1]>nums[i+1]){
+                   nums[i+1]=nums[i];
+               }
+               else {
+                 nums[i]=nums[i+1];
+               }
+                break;
+            } 
+        }
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]>nums[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
