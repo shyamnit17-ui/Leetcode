cfda6191e9cf73e3a3044202babcb0f88bfc1288
Shyam Rathod <shyamnit17@gmail.com>
2026-09-11 22:22:07 +0530

    Time: 1 ms (100%), Space: 43.5 MB (65.61%) - LeetHub

--- 3019-number-of-changing-keys/3019-number-of-changing-keys.java
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.java
@@ -1,13 +1,13 @@
 class Solution {
     public int countKeyChanges(String s) {
-        String st=s.toLowerCase();
+  
         int count=0;
-       int n=1;
-       while(n<s.length()){
-        if(st.charAt(n)!=st.charAt(n-1)){
+       int i=1;
+       while(i<s.length()){
+        if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(i - 1))){
             count++;
         }
-        n++;
+        i++;
        }
         return count;
     }
